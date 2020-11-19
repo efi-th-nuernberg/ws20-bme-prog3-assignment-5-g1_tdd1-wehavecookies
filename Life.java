d7da3c716b0d76bcb7ee162d10666cef3b858c85
repl.it user <replituser@example.com>
2020-11-19 16:17:32 +0000

    commiting finished version of ex5

--- Life.java
+++ Life.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 
 public class Life implements ILife {
   public static boolean[][] Alive = new boolean[5][5];
+  public static int[][] Neighbors = new int[5][5];
 
   public static void main(String[] args) {
     Life l = new Life(new String[] {  "     ",
@@ -30,7 +31,9 @@ public class Life implements ILife {
   @Override
   public void nukeAll() {
     // TODO Auto-generated method stub
-
+    for (int y = 0; y < 5; y++)
+      for (int x = 0; x < 5; x++)
+      setDead(Alive[x][y]); 
   }
 
   @Override
@@ -40,42 +43,71 @@ public class Life implements ILife {
 
   @Override
   public void setDead(int x, int y) {
-    // TODO Auto-generated method stub
-
+    Alive[x][y] = false;
   }
 
   @Override
   public boolean isAlive(int x, int y) {
-    // TODO Auto-generated method stub
     return Alive[x][y];
   }
 
   @Override
   public ILife nextGeneration() {
+
+    // Generiert neue Generation
+    ILife nGen = new Life();
+
+    // 4 Fälle müssen für die nächste Generation gecheckt werden 
+    // Fall 1: Eine neue Zelle entsteht wenn genau 3 Nachbarn vorhanden sind
+    // Fall 2: Eine Zelle stirbt wenn weniger als 2 Nachbarn vorhanden sind
+    // Fall 3: Eine Zelle überlebt wenn 2 oder 3 Nachbarn vorhanden sind
+    // Fall 4: Eine Zelle stirbt wenn mehr als 3 Nachbarn vorhanden sind
+
     // TODO Auto-generated method stub
+
+    calculateNeighbors();
+
     for (int y = 0; y < 5; y++)
       for (int x = 0; x < 5; x++)
-      if (isAlive(x,y))
       {
-        //check is still alive
-          //Mit weniger als 2 Nachbarn stirbt die Zelle
-          //Mit mehr als 3 Nachbarn stirbt die Zelle
-      } else {
+        int currentneighbors = Neighbors[x][y];
+        if (isAlive(x,y))
+        {
+          //check is still alive
+            // Checke Fall 2 und 4
+            if(currentneighbors < 2 || currentneighbors > 3){
+              setDead(x, y);
+            }
+            // Checke Fall 3
+            if(currentneighbors == 2 || currentneighbors == 3){
+              setAlive(x, y);
+            }
         //check if now alive
-          //rule 1: 3 neighbors
-        int neighbors = 0;
-        for (int i = 0; i<3; i++)
-          for (int j = 0; j<3; j++)
-          if (x+i-1 >= 0 && x+i-1 <= 4 && y+j-1 >= 0 && y+j-1 <= 4)
-            if(isAlive(x+i-1, y+j-1))
-              neighbors++;
-        if(neighbors>=3)
-          setAlive(x,y);
-            
-        
+        // Checke Fall 1
+        } else if (currentneighbors == 3){
+            setAlive(x, y);
+        }
       }
     return nGen;
   }
+
+ 
+
+  public void calculateNeighbors(){
+    // Berechnen der Nachbarn für jede einzelne Zelle
+    // Code von Erik übernommen
+    for (int y = 0; y < 5; y++)
+      for (int x = 0; x < 5; x++){
+        int neighbors = 0;
+        for (int i = 0; i<3; i++)
+          for (int j = 0; j<3; j++)
+            if (x+i-1 >= 0 && x+i-1 <= 4 && y+j-1 >= 0 && y+j-1 <= 4)
+              if(isAlive(x+i-1, y+j-1))
+                neighbors++;
+                Neighbors[x][y] = neighbors;
+      }
+  }
+
 }
 
 // -cp .:./jars/junit-4.12.jar:./jars/hamcrest-core-1.3.jar org.junit.runner.JUnitCore
--- LifeTest.java
+++ LifeTest.java
@@ -2,7 +2,7 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class LifeTest {
-    
+
     @Test
     public void createNewCell() {
        // Arrange: drei lebende Zellen
@@ -11,6 +11,7 @@ public class LifeTest {
         l.setAlive(0, 1);
         l.setAlive(0, 2);
 
+        System.out.println("Generation 1:\n");
         for (int y = 0; y < 5; y++)
         {
           System.out.print("Zeile: " + y + " || ");
@@ -23,6 +24,7 @@ public class LifeTest {
         // Act: Berechnung der Folgegeneration
         ILife nextGen = l.nextGeneration();
 
+        System.out.println("Generation 2:\n");
         for (int y = 0; y < 5; y++)
         {
           System.out.print("Zeile: " + y + " || ");
@@ -38,16 +40,112 @@ public class LifeTest {
 
     @Test
     public void destroyLonelyCell() {
+      // Arrange: vier lebende Zellen
+        Life l = new Life();
+        l.setAlive(0, 0);
+        l.setAlive(0, 1);
+        l.setAlive(0, 2);
+        l.setAlive(4, 1);
+
+        System.out.println("Generation 3:\n");
+        for (int y = 0; y < 5; y++)
+        {
+          System.out.print("Zeile: " + y + " || ");
+          for (int x = 0; x < 5; x++)
+             System.out.print(x + " " + l.isAlive(x,y) + " ");
+          System.out.println("\n");
+        }
+        System.out.println("\n");
+
+        // Act: Berechnung der Folgegeneration
+        ILife nextGen = l.nextGeneration();
+
+        System.out.println("Generation 4:\n");
+        for (int y = 0; y < 5; y++)
+        {
+          System.out.print("Zeile: " + y + " || ");
+          for (int x = 0; x < 5; x++)
+             System.out.print(x + " " + l.isAlive(x,y) + " ");
+          System.out.println("\n");
+        }
+
+        // Assert: Rasterpunkt mit drei Nachbarn sollte jetzt leben
+        assertFalse(nextGen.isAlive(4, 1));
     }
 
 
     @Test
     public void keepAliveCell() {
+      // Arrange: vier lebende Zellen
+        Life l = new Life();
+        l.setAlive(2,3);
+        l.setAlive(3, 2);
+        l.setAlive(3, 3);
+
+        System.out.println("Generation 5:\n");
+        for (int y = 0; y < 5; y++)
+        {
+          System.out.print("Zeile: " + y + " || ");
+          for (int x = 0; x < 5; x++)
+             System.out.print(x + " " + l.isAlive(x,y) + " ");
+          System.out.println("\n");
+        }
+        System.out.println("\n");
+
+        // Act: Berechnung der Folgegeneration
+        ILife nextGen = l.nextGeneration();
+
+        System.out.println("Generation 6:\n");
+        for (int y = 0; y < 5; y++)
+        {
+          System.out.print("Zeile: " + y + " || ");
+          for (int x = 0; x < 5; x++)
+             System.out.print(x + " " + l.isAlive(x,y) + " ");
+          System.out.println("\n");
+        }
+
+        // Assert: Rasterpunkt mit drei Nachbarn sollte jetzt leben
+        assertTrue(nextGen.isAlive(2, 3));
+
     }
 
 
     @Test
     public void destroyCrowdedCell() {
+    // Arrange: vier lebende Zellen
+        Life l = new Life();
+        l.setAlive(0, 0);
+        l.setAlive(0, 1);
+        l.setAlive(1, 0);
+        l.setAlive(1, 1);
+        l.setAlive(2, 0);
+        l.setAlive(2, 1);
+
+        System.out.println("Generation 7:\n");
+        for (int y = 0; y < 5; y++)
+        {
+          System.out.print("Zeile: " + y + " || ");
+          for (int x = 0; x < 5; x++)
+             System.out.print(x + " " + l.isAlive(x,y) + " ");
+          System.out.println("\n");
+        }
+        System.out.println("\n");
+
+        // Act: Berechnung der Folgegeneration
+        ILife nextGen = l.nextGeneration();
+
+        System.out.println("Generation 8:\n");
+        for (int y = 0; y < 5; y++)
+        {
+          System.out.print("Zeile: " + y + " || ");
+          for (int x = 0; x < 5; x++)
+             System.out.print(x + " " + l.isAlive(x,y) + " ");
+          System.out.println("\n");
+        }
+
+        // Assert: Rasterpunkt mit drei Nachbarn sollte jetzt leben
+        assertFalse(nextGen.isAlive(0, 1));
+
     }
 
 
